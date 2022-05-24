fe6578c4a5a711fbf061ccdcfdf776c2df96078f
disher.io <drew@disher.io>
2022-05-24 11:39:55 -0400

    wrote first hello world program

---
+++ 0-Hello_World/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+		
+		int num1 = 7;
+		int num2 = 8;
+
+		System.out.println(num1 + num2);
+	}
+
+}
