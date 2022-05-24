6e639fa92c2c8adb508349d1fd012449db876a12
disher.io <drew@disher.io>
2022-05-24 12:13:35 -0400

    data types

---
+++ 1-Data_Types/src/PrimitiveDataTypes.java
@@ -0,0 +1,32 @@
+
+public class PrimitiveDataTypes {
+	public static void main(String[] args) {
+		// -128 to 127
+		byte myByte = 12;
+		
+		// -32768 to 32,767
+		short myShort = 1234;
+		
+		// -2,147,483,648 to 2,147,483,647
+		int myInt = 2837;
+		
+		// -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+		long myLong = 12323;
+		
+		// about 6 decimal digits
+		float myFloat = 6.3f;
+		
+		// about 15 decimal digits;
+		double myDouble = 45.213434;
+		
+		// true or false
+		boolean myBool = true;
+		boolean moreThan1000 = (myInt > 1000);
+		
+		// one character
+		// chars are single quoted, strings are doublequoted
+		char myChar = 'W';
+		
+		
+	}
+}
